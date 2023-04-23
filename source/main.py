82c378d769c2b9798ddcc92afd450c92a01aa93c
marcmg98 <129801453+marcmg98@users.noreply.github.com>
2023-04-23 23:39:48 +0200

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,94 +1,101 @@
-import os
-import requests
-import csv
 from bs4 import BeautifulSoup
-
-URL = "https://www.zara.com/es"
-
-# Se cambia el User-Agent para evitar que nos deneguen el permiso
-headers = {
-    'User-Agent': 'Mozilla/5.0'
-}
-
-response = requests.post(URL, headers=headers)
-soup = BeautifulSoup(response.text,"html.parser")
-
-# Encontrar la sección que contiene las categorías de ropa
-categorias = soup.find('div', {'class': "layout layout--grid-type-full layout-error layout-error--background-in-desktop"})
-print(categorias)
-
-"""
-parece que llega un nivel de produndidad que, al hacer web scapping, se reemplaza el contenido por:
-
-<path clip-rule="evenodd" d="
-    M105.673.035l19.557 53.338 6.77.002v.383h-21.548v-.383h6.344l-6.431-17.54H97.311v.007l.07.204c.521 1.548.78 
-    3.17.764 4.803v6.575c0 3.382 1.494 6.81 4.347 6.81 1.675 0 3.012-.59 4.604-2.046l.227.211C105.594 54.224 
-    103.5 55 100.36 55c-2.37 0-4.398-.57-6.03-1.693l-.309-.222c-2.148-1.624-3.542-4.278-4.142-7.89l-.096-.583-.1
-    -.882-.01-.152-3.599 9.792h5.107v.384H80.496v-.384h5.162l3.951-10.753v-.023a34.924 34.924 0 0 1-.075-1.906v
-    -4.693c0-5.77-4.29-9.08-11.771-9.08H70.41v26.458h6.371v.383h-24.9v-.383h6.345l-6.431-17.54H33.948l-6.371 
-    17.346.266-.044c8.366-1.442 12.213-7.827 12.265-14.55h.388v15.171H0L30.06 2.185H17.972C7.954 2.185 3.42 
-    9.922 3.35 17.635h-.387V1.8h36.488l-.222.385L9.396 53.373h15.695c.39 0 .778-.019 1.169-.05.26-.018.522-.044.788
-    -.077l.095-.01L46.703 0h.387l.013.035 15.369 41.916V2.185h-6.328v-.39h21.778c10.467 0 17.774 5.372 17.774 13.068 
-    0 5.612-5.005 10.27-12.45 11.595l-1.367.174 1.377.14c4.515.517 8.1 1.906 10.641 4.127l.017.016L105.273 
-    0h.386l.014.035zm-8.552 35.32l.038.094h13.061l-8.773-23.928-7.221 19.67.039.037.367.364a11.876 11.876 0 0 1 2.489 
-    3.762zM70.415 26.53V2.185h5.611c7.496 0 11.454 4.414 11.454 12.76 0 8.877-2.272 11.585-9.717 11.585h-7.348zM42.882 
-    11.521L34.09 35.45h17.565L42.882 11.52z" 
-fill-rule="evenodd"></path>
-
-"""
-
-# # Obtener las URLs de las categorías de ropa
-# URLs = []
-# for categoria in categorias.find_all('a'):
-#     categoria_url = categoria.get('href')
-#     if categoria_url is not None:
-#         URLs.append(categoria_url)
-
-# # Imprimir las# URLs de las categorías de ropa
-# print(URLs)
-
-#products = soup.find_all('article', {'data-test-id': 'productListItem'})
-
-#TEST1
 import requests
-from bs4 import BeautifulSoup
-import csv
+import pandas as pd
+import numpy as np
+import time
 
-url = 'https://www.zara.com/es'
-page = requests.get(url)
+# En primer lugar, cambiamos los headers para evitar ser identificados como script y no nos bloqueen.
+headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,\*/*;q=0.8", 
+           "Accept-Encoding": "gzip, deflate, sdch, br",
+           "Accept-Language": "en-US,en;q=0.8",
+           "Cache-Control": "no-cache",
+           "dnt": "1",
+           "Pragma": "no-cache",
+           "Upgrade-Insecure-Requests": "1",
+           "User-Agent": "Mozilla/5.0"
+          }
+sitio_web = "https://preciosmundi.com"
+sitio_web_xml = "https://preciosmundi.com/sitemap.xml"
 
-soup = BeautifulSoup(page.content, 'html.parser')
+def extraer_soup(website, header):
+# Extraemos directamente la información en formato xml del sitemap. 
+# Cargamos la información del sitio usando BeautifulSoup:
+    web = requests.get(website,headers=header) # Obtenemos la información del sitemap
+    return BeautifulSoup(web.text,'xml') # Creamos un objeto soup para facilitar la navegación del xml
+    
+soup = extraer_soup(sitio_web_xml, headers)
 
-with open('../dataset/mujer.csv', mode='w', newline='', encoding='utf-8') as mujer_file:
-    mujer_writer = csv.writer(mujer_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    mujer_writer.writerow(['Name', 'Availability', 'Price', 'Product Code', 'Color'])
+# Vemos que las páginas web van entre dos etiquetas <loc> </loc> y dos etiquetas <url> </url>:
 
-with open('../dataset/hombre.csv', mode='w', newline='', encoding='utf-8') as hombre_file:
-    hombre_writer = csv.writer(hombre_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    hombre_writer.writerow(['Name', 'Availability', 'Price', 'Product Code', 'Color'])
+urls = soup.find_all("loc") # Buscamos todos los loc
+print(urls)
 
-products = soup.find_all('span', class="layout-categories-category__name")
+# Ahora obtenemos el texto que está entre las dos etiquetas (las diferentes URL) y lo guardamos en una lista.
+url_list=[]
+for a in urls:
+    url_list.append(a.get_text())
 
-"""for product in products:
-    gender = product.find('a', {'data-test-id': 'product-link'}).get('href').split('/')[-3]
-    if gender == 'mujer':
-        name = product.find('a', {'data-test-id': 'product-link'}).get('aria-label')
-        availability = product.find('div', {'class': 'product-status'}).text.strip()
-        price = product.find('span', {'class': 'price'}).text.strip()
-        product_code = product.find('div', {'class': 'product-reference'}).text.strip()
-        color = product.find('div', {'class': 'product-colors'}).text.strip()
-        with open('../dataset/mujer.csv', mode='a', newline='', encoding='utf-8') as mujer_file:
-            mujer_writer = csv.writer(mujer_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            mujer_writer.writerow([name, availability, price, product_code, color])
-    elif gender == 'hombre':
-        name = product.find('a', {'data-test-id': 'product-link'}).get('aria-label')
-        availability = product.find('div', {'class': 'product-status'}).text.strip()
-        price = product.find('span', {'class': 'price'}).text.strip()
-        product_code = product.find('div', {'class': 'product-reference'}).text.strip()
-        color = product.find('div', {'class': 'product-colors'}).text.strip()
-        with open('../dataset/hombre.csv', mode='a', newline='', encoding='utf-8') as hombre_file:
-            hombre_writer = csv.writer(hombre_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            hombre_writer.writerow([name, availability, price, product_code, color])
-"""
+# Nos paramos a observar los 20 primeros elementos de la lista de url's para ver cuáles debemos descartar.
+print(url_list[1:21])
 
-print(products)
+# Los 17 primeros elementos de la listan no nos interesan para este trabajo en particular, dado que solo queremos obtener tablas de precios en los distintos países. Por tanto, los eliminamos.
+del url_list[0:18]
+
+print(url_list[1:21])
+# Podemos ver que cada país tiene 7 páginas web diferentes donde está almacenada la información. El objetivo de ese trabajo es extraer los datos de precios de ropa y calzado para todos los países que aparecen en esta página web.
+len(url_list) 
+
+# Existen 980 elementos en la lista, divididos en 140 grupos de 7 elementos cada uno (País+super+restaurante+ropa+calzado+transporte+vivienda+ocio). 
+# Por lo tanto, hay 140 países diferentes. Seleccionamos sólo las url de precios de ropa y calzado, aprovechando que están estructurados de manera periódica.
+# Como todos los países tienen las mismas páginas y hay 140 países, podemos coger la tercera página de cada grupo de 7.
+url_subList = [url_list[n+3] for n in range(0, len(url_list), 7)] 
+url_subList
+print(url_subList)
+
+def dinero_ropa_pais(url,header):
+    web = requests.get(url,headers=header) # Obtenemos la página web
+    soup_real = BeautifulSoup(web.text,'html') # Tranformamos en objeto soup
+    data_url = soup_real.find('div',{'class':'table-responsive-md'}) #Buscamos la tabla, esto lo hicimos con ayuda de un navegador
+    
+    country=url.replace('/', ' ').split()[2].capitalize() #Cogemos el país de la url y le ponemos la primera letra mayúscula
+    columns=['Unos zapatos de hombre de cuero',
+       'Unas zapatillas deportivas de marca (Nike, Adidas, Puma, etc.)',
+       'Un vestido (Zara, H&M, etc.)',
+       'Unos vaqueros Levis 501 (o equivalente)'] #Nombres de las columnas de precios de vivienda
+    
+    if data_url != None: #En el caso de que la página web tenga una tabla hacemos esto
+        real_state_table= str(data_url)
+        df_temp = pd.read_html(real_state_table)
+        df_temp = df_temp[0]
+        df_temp['DÃ³lar ($)'] = df_temp['DÃ³lar ($)'].str.replace('$', '') #Cambios de estilo en el dataframe
+        df_temp['DÃ³lar ($)'] = df_temp['DÃ³lar ($)'].str.replace(',', '.')
+        df_temp['DÃ³lar ($)'] = df_temp['DÃ³lar ($)'].astype(float)
+        selected_columns = df_temp[['Producto','DÃ³lar ($)']] #Mantenemos sólo el precio en dólares
+        df_temp2 = selected_columns.copy()
+        df_temp2.columns = ['Producto','Precio en dólares'] #Cambiamos el nombre para que quede más presentable
+        df_temp3=df_temp2.transpose() #Queremos cada país como una fila, y esta al revés. Transponemos
+        df_temp3.columns=list(df_temp3.iloc[0]) #Al trasponer el nombre de las columnas es una fila del dataframe
+        df_temp3=df_temp3.drop(df_temp3.index[0]) #Lo solucionamos y guardamos el nombre
+        df_temp4 = df_temp3.rename(index={'Precio en dólares':country}) #Renombramos la fila al nombre del país
+        time.sleep(0.5) #Para evitar hacer muchas peticiones al sitio, obligamos a parar 0.5 segundos después de cada petición.
+        return(df_temp4)
+    
+    else: #Si no tiene tabla creamos un dataframe con nans para retornarlo
+        print('Sin información de precios de ropa y calzado en '+ country)
+        df_null=pd.DataFrame(np.zeros([1, 4])*np.nan).rename(index={0:country})
+        df_null.columns=columns
+        return(df_null)
+    
+
+df = pd.DataFrame() 
+for url in url_subList:
+    df_temp=dinero_ropa_pais(url,headers)
+    df=pd.concat([df,df_temp])
+    
+df.columns=['Unos zapatos de hombre de cuero (USD)',
+       'Unas zapatillas deportivas de marca (Nike, Adidas, Puma, etc.) (USD)',
+       'Un vestido (Zara, H&M, etc.) (USD)',
+       'Unos vaqueros Levis 501 (o equivalente) (USD)']
+
+print(df)
+df.to_csv('PreciosRopaPorPais.csv')
